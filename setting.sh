527947fe862e20aa44c9bfe89dd106e1fd83ea21
jasonli <lrj_scorpio@sina.com>
2018-08-26 14:37:11 +0800

    add test directory

---
+++ setting.sh
@@ -0,0 +1,14 @@
+#!/usr/bin
+
+git config --global user.name "jasonli"
+git config --global user.email "lrj_scorpio@sina.com"
+
+#设置命令别名
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.unstage "reset HEAD --"
+
+#移除命令别名
+#git config --global --unset alias.st status
